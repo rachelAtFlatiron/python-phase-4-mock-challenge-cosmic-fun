c9a9d848f1a736fc29ae0d8916ff0cd3a1a02fe9
Rachel Katz <rachel.katz@flatironschool.com>
2023-06-20 10:15:36 -0400

    most stuff

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, abort
+from werkzeug.exceptions import UnprocessableEntity
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+from flask_restful import Api, Resource 
 from models import db, Scientist, Planet, Mission
 
 app = Flask(__name__)
@@ -14,13 +15,64 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
 
 @app.route('/')
 def index():
     response = make_response(
         {"message": "Hello Scientists!"}
     )
+    return response 
+
+class Scientists(Resource):
+    def get(self): 
+        q = Scientist.query.all()
+        if not q: 
+            abort(404, "Scientist not found")
+        return make_response([s.to_dict(only=('id', 'name', 'field_of_study', 'avatar')) for s in q], 200)
+    
+    def post(self):
+        data = request.get_json()
+        try:
+            s = Scientist(name=data.get('name'), field_of_study=data.get('field_of_study'), avatar=data.get('avatar'))
+            db.session.add(s)
+            db.session.commit()
+        except Exception: 
+            #abort(422, "validation errors")
+            return make_response({"errors": ["validation errors"]}, 422)
+        return make_response(s.to_dict(only=('id', 'name', 'field_of_study', 'avatar')), 201)
+api.add_resource(Scientists, '/scientists')
+
+class OneScientist(Resource):
+    def get(self, id):
+        q = Scientist.query.filter_by(id=id).first()
+        if not q: 
+            abort(404, "Scientist not found")
+        return make_response(q.to_dict(), 200) 
+    def patch(self , id):
+        q = Scientist.query.filter_by(id=id).first()
+        if not q: 
+            abort(404, "Scientist not found")
+        try: 
+            data = request.get_json()
+            for attr in data: 
+                setattr(q, attr, data.get(attr))
+            db.session.add(q)
+            db.session.commit() 
+        except Exception:
+            return make_response({"errors": ["validation errors"]}, 422)
+        return make_response(q.to_dict(only=("id", "name", "field_of_study", "avatar")), 200)
+    def delete(self, id):
+        q = Scientist.query.filter_by(id=id).first()
+        if not q: 
+            abort(404, "Scientist not found")
+        db.session.delete(q)
+        db.session.commit()
+        return make_response({}, 204)
+    
+api.add_resource(OneScientist, '/scientists/<int:id>')
 
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,3 +10,60 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class Scientist(db.Model, SerializerMixin):
+    __tablename__ = "scientists" 
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    name = db.Column(db.String, nullable=False, unique=True)
+    field_of_study = db.Column(db.String, nullable=False)
+    avatar = db.Column(db.String)
+
+    missions = db.relationship('Mission', back_populates='scientist')
+    planets = association_proxy('missions', 'planet')
+
+    serialize_rules = ('-created_at', '-updated_at', '-missions.scientist', '-planets.scientists')
+
+    def __repr__(self):
+        return f'<Scientist {self.id} {self.name} />'
+
+class Planet(db.Model, SerializerMixin):
+    __tablename__ = "planets" 
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    name = db.Column(db.String) 
+    distance_from_earth = db.Column(db.String) 
+    nearest_star = db.Column(db.String) 
+    image = db.Column(db.String) 
+
+    missions = db.relationship('Mission', back_populates='planet')
+    scientists = association_proxy('missions', 'scientist')
+    
+    serialize_rules = ('-created_at', '-updated_at', '-missions.planet', '-scientists.planets')
+
+    def __repr__(self): 
+        f"<Planet {self.id} {self.name} />"
+
+class Mission(db.Model, SerializerMixin):
+    __tablename__ = "missions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    name = db.Column(db.String, nullable=False)
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
+
+    scientist = db.relationship('Scientist', back_populates='missions')
+    planet = db.relationship('Planet', back_populates='missions')
+
+    serialize_rules = ('-created_at', '-updated_at', '-scientist.missions', '-planet.missions')
+    
+    def __repr__(self):
+        return f"<Mission {self.id} {self.name} />"
